aed67c956a8a94ac64d8f6e54979707b5c3dd582
Ng Wei Han <weihan774237@gmail.com>
2023-03-22 13:49:28 +0800

    update config to BSC testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,15 +3,17 @@ import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const INFURA_API_KEY: string = process.env.INFURA_API_KEY ?? '';
+// const INFURA_API_KEY: string = process.env.INFURA_API_KEY ?? '';
 const GOERLI_PRIVATE_KEY: string = process.env.GOERLI_PRIVATE_KEY ?? '';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
-    goerli: {
-      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
+    testnet: {
+      url: 'http://localhost:8645',
+      chainId: 97,
+      accounts: [GOERLI_PRIVATE_KEY],
+      gas: 60000000000
     }
   }
 };
